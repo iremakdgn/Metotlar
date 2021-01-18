0dfc71fc84d285f449484316dc8d6f1ff1eefa28
iremakdgn <irem.akdgn.1996@gmail.com>
2021-01-18 14:38:23 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Açiklama = "Amasya Elması";
+
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Açiklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (var urun  in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Açiklama);
+                Console.WriteLine("-------------");
+
+            }
+            Console.WriteLine("-----------------Metotlar-----------------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+        }
+    }
+}
+//tekrar etmeyen kodları metotlarla yazabiliriz
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("sepete eklendi. : "+ urun.Adi );
+        }
+        public void Ekle2(string urunAdi, string aciklama, double fiyati)
+        {
+            Console.WriteLine("sepete eklendi. : " + urunAdi);
+        }
+    }
+}
