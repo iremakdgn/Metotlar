7f216316abf94497c1efbaac2a1ae9d432224c08
iremakdgn <irem.akdgn.1996@gmail.com>
2021-02-08 16:10:42 +0300

    koleksiyonlar ve generic yapılar eklendi.

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T> //t : programcı ne tip verirse demek
+    {
+        T[] items;
+        public MyList() //constructor
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items; //geçici dizide itemsı tutuyo
+            items = new T[items.Length+1];
+            for(int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoriId = 2;
+            product1.ProductName = "masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product { Id = 2, CategoriId=5, UnitsInStock=5, 
+                ProductName="Kalem", UnitPrice=35 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+            //int, double,float .. sayısal tipler değer tipleri
+            //dizi,class,abstract class,interface.. referans tiptir
+
+        }
+    }
+}
